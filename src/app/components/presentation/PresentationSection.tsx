26ad5038717532e0ad5c880118ca4fa6a291fa61
Steven Reis <steven.oliveira@ciss.com.br>
2024-08-24 01:38:23 -0300

    Ajustado arredondamento foto principal

--- src/app/components/presentation/PresentationSection.tsx
+++ src/app/components/presentation/PresentationSection.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { Button } from "@/components/button/Button"
 import { TextInterface } from "@/texts/text-interface"
 import Image from "next/image"
 import Eu from '../../../../public/eu.jpg'
@@ -9,14 +8,14 @@ export const PresentationSection = (props: { text: TextInterface }) => {
   
   return <section className="h-full flex flex-col items-center justify-center">
     <div className="flex flex-col gap-5 items-center justify-center h-full text-center md:w-3/6">
-      <div className="flex justify-center items-center w-1/2 md:w-1/2 h-1/5 md:h-1/3 relative rounded-full">
+      <div className="flex justify-center items-center w-[10rem] h-[10rem] md:w-[15rem] md:h-[15rem] relative">
           <Image 
             src={Eu}
             alt="Minha foto"
             style={{
               objectFit: 'cover',
-              borderRadius: '200px'
             }}
+            className="rounded-full"
             fill
           />  
         </div>
